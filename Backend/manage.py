249b2f33b05a450df873fa99327fc3e0fcb25150
cankargin515 <can.kargin@sabanciuniv.edu>
2025-11-06 21:55:28 +0300

    Fix: clean merge markers in manage.py

--- Backend/manage.py
+++ Backend/manage.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
     try:
         from django.core.management import execute_from_command_line
@@ -18,17 +14,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == "__main__":
-=======
-import os
-import sys
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
-
-if __name__ == '__main__':
->>>>>>> main
     main()
+
