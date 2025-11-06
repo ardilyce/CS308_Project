83e66c28aa54cb83e16bf7b9c0db89e994cc906b
cankargin515 <can.kargin@sabanciuniv.edu>
2025-11-06 21:48:57 +0300

    Resolve merge: take main versions for settings/urls/wsgi

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
-from django.urls import path
-from .views import signup_view, login_view
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    # Auth endpoints
-    path("auth/signup", signup_view, name="auth-signup"),
-    # (Optional) Login endpoint already implemented in views
-    path("auth/login", login_view, name="auth-login"),
-]
-=======
 from django.contrib import admin
 from django.urls import path,include
 from django.http import JsonResponse
@@ -47,4 +17,3 @@ urlpatterns = [
 ]
 
 
->>>>>>> main
--- Backend/backend/wsgi.py
+++ Backend/backend/wsgi.py
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-=======
 # Backend/backend/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
->>>>>>> main
 
 application = get_wsgi_application()
