890c47fdbe683a92320632b4eb7714b586bb1aba
ezgi-kilinc <ezgi.kilinc@sabanciuniv.edu>
2025-11-29 18:46:52 +0300

    Removed missing MainLayout and fixed duplicate routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import MainLayout from "./components/MainLayout.jsx";
+// import MainLayout from "./components/MainLayout.jsx"; // REMOVED: This file is missing and causing the crash
 
 import HomePage from "./pages/HomePage.jsx";
 import SearchResults from "./pages/SearchResults.jsx";
@@ -11,9 +11,6 @@ import FavoritesPage from "./pages/FavoritesPage.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import SupportAgentPage from "./pages/SupportAgentPage";
 import ProductManagerPage from "./pages/ProductManagerPage";
-
-
-
 import ProfilePage from "./pages/ProfilePage.jsx";
 import OrderHistoryPage from "./pages/OrderHistoryPage.jsx";
 import OrderDetailPage from "./pages/OrderDetailPage.jsx";
@@ -21,26 +18,24 @@ import OrderDetailPage from "./pages/OrderDetailPage.jsx";
 export default function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
+        {/* We removed the MainLayout wrapper for now to fix the crash */}
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path="/product/:id" element={<ProductDetailPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
+        
+        {/* Profile Routes */}
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile/orders" element={<OrderHistoryPage />} />
         <Route path="/profile/orders/:orderId" element={<OrderDetailPage />} />
-      </Route>
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/product/:id" element={<ProductDetailPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="/support-agent" element={<SupportAgentPage />} />
-      <Route path="/product-manager" element={<ProductManagerPage />} />
+        {/* Auth & Admin Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/support-agent" element={<SupportAgentPage />} />
+        <Route path="/product-manager" element={<ProductManagerPage />} />
     </Routes>
   );
 }
